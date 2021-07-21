c0a0344c385d3e2d7069b59f4881c15245a9aee6
franckalainai <franckalain.ai@gmail.com>
2021-07-21 16:24:47 +0000

    initial graphical http client

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@showWelcomePage')->name('welcome');
 
-Auth::routes();
+Auth::routes(['register' => false, 'reset' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
