4b5afe35065e98fc0fc45c3056aaed5a331ffe06
franckalainai <franckalain.ai@gmail.com>
2021-07-22 10:58:06 +0000

    automating the obtaining of access token

---
+++ app/Services/MarketAuthenticationService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalServices;
+use App\Traits\InteractsWithMarketResponses;
+
+class MarketAuthenticationService
+{
+    use ConsumesExternalServices, InteractsWithMarketResponses;
+
+    // the URL to send the request
+    protected $baseUri;
+    protected $clientId;
+    protected $clientSecret;
+    protected $passwordClientId;
+    protected $passwordClientSecret;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.market.base_uri');
+        $this->clientId = config('services.market.client_id');
+        $this->clientSecret = config('services.market.client_secret');
+        $this->passwordClientId = config('services.market.password_client_id');
+        $this->passwordClientSecret = config('services.market.password_client_secret');
+    }
+
+    // obtain an access token associated with the client
+    public function getClientCredentialsToken()
+    {
+        if ($token = $this->existingValidToken()) {
+            return $token;
+        }
+
+        $formParams = [
+            'grant_type' => 'client_credentials',
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret,
+        ];
+
+        $tokenData = $this->makeRequest('POST', 'oauth/token', [], $formParams);
+
+        $this->storeValidToken($tokenData, 'client_credentials');
+
+        return $tokenData->access_token;
+    }
+
+    // store a valid token with some attributes
+    public function storeValidToken($tokenData, $grantType)
+    {
+        $tokenData->token_expires_at = now()->addSeconds($tokenData->expires_in - 5);
+        $tokenData->access_token = "{$tokenData->token_type} {$tokenData->access_token}";
+        $tokenData->grant_type = $grantType;
+
+        session()->put(['current_token' => $tokenData]);
+    }
+
+    // verify if there is any valid token on session
+    public function existingValidToken()
+    {
+        if (session()->has('current_token')) {
+            $tokenData = session()->get('current_token');
+            if (now()->lt($tokenData->token_expires_at)) {
+                return $tokenData->access_token;
+            }
+        }
+
+        return false;
+    }
+}
